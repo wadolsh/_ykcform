ac0a5d89e70860b29e653395b68ca8f16a1af6c6
SUNGHO CHOI <wadolsh@yahoo.co.jp>
2014-03-19 02:06:12 -0400

    save

--- server/method.login.js
+++ server/method.login.js
@@ -37,3 +37,27 @@ exports.loginChecker = function(reqData, callback, req){
 	console.log('loginChecker!');
     callback(req.session.user || {});
 };
+
+
+exports.signup = function(reqData, callback, req){
+    mongodb.MongoClient.connect(exports.methodConfig.db.url, function(err, db) {
+        if(err) throw err;
+        db.collection(exports.methodConfig.db.user_dataName)
+            .findOne({login_id : reqData.data.login_id}, function (err, doc) {
+
+            if(err) throw err;
+            
+            if (doc != null) {
+                callback({result: false, msg : "仕様されているIDです。"});
+            } else {
+                if(err) throw err;
+                // id採番
+                reqData.data[idName] = newId();
+                db.collection(exports.methodConfig.db.user_dataName).insert(reqData.data, {w:1}, function (err, docs) {
+                    if(err) throw err;
+                    callback(docs[0]);
+                });
+            }
+        });
+    });
+};
